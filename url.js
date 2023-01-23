2c003684d73db0c96cf1700107c4c7c66a00ad80
Cheng-Jiuan <77944202+Cheng-Jiuan@users.noreply.github.com>
2023-01-23 22:12:18 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
+	'https://forms.gle/5MsPDgU534RWfkPP7',
 	'https://forms.gle/hrK3CPV9dCUDXFNHA',
-	'https://forms.gle/KdQsY9LHRhXuZJee9',
 	'https://forms.gle/5ztq3FxBpXx6m49N9',
 ];
